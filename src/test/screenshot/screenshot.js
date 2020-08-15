5083548974973473b029ed979b51abef50dff775
itizawa <itizawa@weseek.co.jp>
2020-08-15 12:49:07 +0900

    compare diff

--- src/test/screenshot/screenshot.js
+++ src/test/screenshot/screenshot.js
@@ -1,3 +1,6 @@
+const resemble = require('resemblejs');
+const fs = require('fs-extra');
+
 /* eslint-disable no-undef */
 beforeAll(async() => {
   await page.goto('https://qiita.com/');
@@ -7,8 +10,22 @@ afterAll(async(done) => {
   done();
 });
 
-it('トップページ', async() => {
+it('Top page', async() => {
   await page.screenshot({ path: 'src/test/screenshot/after/top.png' });
 
-  await expect(page.title()).resolves.toMatch('Qiita');
+  const imageBefore = fs.readFileSync('src/test/screenshot/before/top.png');
+  const imageAfter = fs.readFileSync('src/test/screenshot/after/top.png');
+
+  let misMatchPercentage = 0;
+
+  // Compare the difference between before and after
+  resemble(imageAfter).compareTo(imageBefore)
+    .ignoreColors()
+    .onComplete((data) => {
+      fs.writeFileSync('src/test/screenshot/diff/top.png', data.getBuffer());
+      misMatchPercentage = data.misMatchPercentage;
+    });
+
+  // Difference tolerance
+  expect(misMatchPercentage).toBeLessThan(1);
 });
